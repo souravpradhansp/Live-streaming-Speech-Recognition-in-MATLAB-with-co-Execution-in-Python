c824774525454e816716fb286de542802a391436
souravpradhansp <127275612+souravpradhansp@users.noreply.github.com>
2023-04-24 13:03:39 -0400

    Update Live_Stream_Speech_Recognition.py

--- HelperFiles/Live_Stream_Speech_Recognition.py
+++ HelperFiles/Live_Stream_Speech_Recognition.py
@@ -5,39 +5,30 @@ import torch
 import torchaudio
 
 class SpeechRecognizer():
-    
     def __init__(self):
         super(SpeechRecognizer, self).__init__()
         bundle = torchaudio.pipelines.EMFORMER_RNNT_BASE_LIBRISPEECH
-        self.bundle = bundle
         self.streaming_feature_extractor = bundle.get_streaming_feature_extractor()
         self.decoder = bundle.get_decoder()
         self.token_processor = bundle.get_token_processor()
-    
         self.beam_width = 10
-    
         self.state = None
         self.hypothesis = None
 
-        self.segment_length = self.bundle.segment_length * self.bundle.hop_length
-        self.context_length = self.bundle.right_context_length * self.bundle.hop_length
-        self.context = torch.zeros([self.context_length])
-
-    def cacher(self, chunk: torch.Tensor):
-        chunk = torch.Tensor(chunk)
-
-        if chunk.size(0) < self.segment_length:
-            chunk = torch.nn.functional.pad(chunk, (0, self.segment_length - chunk.size(0)))
-        chunk_with_context = torch.cat((self.context, chunk))
-        self.context = chunk[-self.context_length :]
-        return chunk_with_context
-
-    def streamingInfer(self, chunk: torch.Tensor) -> str:
-        segment = self.cacher(chunk)
+    def streamingInfer(self, segment: torch.Tensor):
+        segment = torch.Tensor(segment)
         features, length = self.streaming_feature_extractor(segment)
         hypos, self.state = self.decoder.infer(
             features, length, self.beam_width, state=self.state, hypothesis=self.hypothesis
             )
         self.hypothesis = hypos[0]
         transcript = self.token_processor(self.hypothesis[0], lstrip=False)
+        return transcript
+
+    def nonStreamingInfer(self, segment: torch.Tensor):
+        segment = torch.Tensor(segment)
+        with torch.no_grad():
+            features, length = self.non_streaming_feature_extractor(segment)
+            hypotheses = self.decoder(features, length, 10)
+        transcript = self.token_processor(hypotheses[0][0])
         return transcript
